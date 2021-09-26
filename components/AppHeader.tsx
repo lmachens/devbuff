d40e99728fead28f8c2530f6f12f176e89614a5b
lmachens <leon@machens.koeln>
2021-09-26 14:33:35 +0200

    🍱 Add logo

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -1,11 +1,13 @@
 import styles from "../styles/AppHeader.module.css";
 import Link from "next/link";
+import Image from "next/image";
 
 function AppHeader() {
   return (
     <header className={styles.container}>
       <Link href="/">
         <a className={styles.link}>
+          <Image src="/logo.png" alt="" width={48} height={48} />
           <h1>DevBuff</h1>
         </a>
       </Link>
