3dba386c2911afcf2f7f993516e1e4273f78e025
lmachens <leon.machens@googlemail.com>
2021-10-01 19:52:52 +0200

    💄 Add light theme

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -1,16 +1,21 @@
 import styles from "../styles/AppHeader.module.css";
 import Link from "next/link";
 import Image from "next/image";
+import dynamic from "next/dynamic";
+import LogoSrc from "../public/logo.png";
+
+const ThemeToggleAsync = dynamic(() => import("./ThemeToggle"), { ssr: false });
 
 function AppHeader() {
   return (
     <header className={styles.container}>
       <Link href="/">
         <a className={styles.link}>
-          <Image src="/logo.png" alt="" width={48} height={48} />
+          <Image src={LogoSrc} alt="" width={48} height={48} className="logo" />
           <h1>DevBuff</h1>
         </a>
       </Link>
+      <ThemeToggleAsync />
     </header>
   );
 }
---
+++ components/ThemeToggle.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import darkModeSrc from "../public/dark-mode.svg";
+import lightModeSrc from "../public/light-mode.svg";
+import styles from "../styles/ThemeToggle.module.css";
+
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+function ThemeToggle() {
+  const [darkMode, setDarkMode] = useState(() => prefersDarkScheme.matches);
+
+  useEffect(() => {
+    function handler(event: MediaQueryListEvent) {
+      document.body.classList.remove("dark");
+      document.body.classList.remove("light");
+      setDarkMode(event.matches);
+    }
+    prefersDarkScheme.addEventListener("change", handler);
+    return () => prefersDarkScheme.removeEventListener("change", handler);
+  }, []);
+
+  function handleToggle() {
+    if (darkMode) {
+      document.body.classList.add("light");
+      document.body.classList.remove("dark");
+    } else {
+      document.body.classList.add("dark");
+      document.body.classList.remove("light");
+    }
+    setDarkMode(!darkMode);
+  }
+
+  return (
+    <button onClick={handleToggle} className={styles.button}>
+      <Image
+        src={darkMode ? lightModeSrc : darkModeSrc}
+        alt={darkMode ? "Heller" : "Dunkler"}
+      />
+    </button>
+  );
+}
+
+export default ThemeToggle;
