ac054ba2e4dc286656cfb734f2b2e75dad621f91
lmachens <leon@machens.koeln>
2021-10-04 20:36:38 +0200

    ✏️ Fix typo

--- components/Comparison.tsx
+++ components/Comparison.tsx
@@ -13,11 +13,11 @@ function Comparison() {
       <div className={styles.comparison}>
         <section className={styles.bad}>
           <h3>Andere Bootcamps</h3>
-          <div className={styles.price}>7.500 € - 10.000€</div>
+          <div className={styles.price}>7.500 € - 10.000 €</div>
           <ul>
             <li>10-20 Teilnehmer*innen</li>
             <li>Keine Wahl der Coaches</li>
-            <li>Generisches Kursinhalte</li>
+            <li>Generische Kursinhalte</li>
             <li>Strenge Verträge</li>
             <li>Intransparentes Geschäftsmodell</li>
             <li>Fragwürdige Datenschutzbedingungen</li>
@@ -26,7 +26,7 @@ function Comparison() {
         </section>
         <section className={styles.good}>
           <h3>DevBuff</h3>
-          <div className={styles.price}>5.000 € - 7.500€</div>
+          <div className={styles.price}>5.000 € - 7.500 €</div>
           <ul>
             <li>5-10 Teilnehmer*innen</li>
             <li>Du weißt bei welchem Coach du starten wirst</li>
